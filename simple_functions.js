9fd35a124a4dd327353a24804c646734622fc0e8
Daniel Hernandez <dh893177@gmail.com>
2024-04-30 13:04:50 -0500

    completed simple_functions.js

---
+++ simple_functions.js
@@ -0,0 +1,22 @@
+"use strict"
+
+function favoriteThings(){
+    console.log("Daniel")
+    console.log("Spiderman")
+    console.log("Midland")
+}
+
+//calling our fucntions
+favoriteThings();
+
+function whyImHere (){
+    console.log("i joined the learn to code for the bread")
+}
+
+whyImHere();
+
+function favoritePlace(){
+    console.log("my favorite place to visit is mexico because i have a lot of family over there")
+}
+
+favoritePlace();
